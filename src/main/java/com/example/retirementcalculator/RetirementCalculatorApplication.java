28d59a2976dc8e040eabe8ca5f831c477554266e
Hsiao-young <luxiaoyong95@gmail.com>
2024-09-13 19:15:43 +0800

    Create RetirementCalculatorApplication.java

---
+++ src/main/java/com/example/retirementcalculator/RetirementCalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.example.retirementcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetirementCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetirementCalculatorApplication.class, args);
+    }
+}
